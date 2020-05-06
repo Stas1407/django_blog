bbb55ef7d6c8bc533192264f02abf64bb5abce35
Stas1407 <stanislaw.rajm@gmail.com>
2020-05-06 19:33:47 +0200

    Post add, remove and update futures were added.

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.PostListView.as_view(), name="home"),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name="detail"),
+    path('post/new/', views.PostCreateView.as_view(), name="create"),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name="update"),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name="delete"),
     path('about/', views.about, name="about"),
 ]
